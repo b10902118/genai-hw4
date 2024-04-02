dffbd2d5e137f3b961a848f05b29bdb3b7dec869
b10902118 <b10902118@linux1.csie.ntu.edu.tw>
2024-04-02 09:51:48 +0800

    unpkl conpatable

--- unpkl.py
+++ unpkl.py
@@ -5,12 +5,36 @@ pkl_files = glob.glob("./*.pkl")
 
 hard_cnt = [0] * 30
 
+data = {}
+
 for filename in pkl_files:
     prompt_template_str, trials, res_list, res_stats_str = pickle.load(
         open(filename, "rb")
     )
-    print(filename)
-    print(res_list[7])
+    # print(res_list)
+    # convert old format to new
+    if isinstance(res_list[0], str):
+        # Get the length of each part
+        part_len = len(res_list) // 3
+        # Split res_list into three equal parts
+        res_list = [
+            res_list[i : i + part_len] for i in range(0, len(res_list), part_len)
+        ]
+    data[filename] = (prompt_template_str, trials, res_list, res_stats_str)
+
+
+while True:
+    n = int(input("Number of problem to check: "))
+    for filename, (
+        prompt_template_str,
+        trials,
+        res_list,
+        res_stats_str,
+    ) in data.items():
+        print(filename,'\n')
+        for i in range(3):
+            print(res_list[i][n])
+            input("press enter to continue")
 
 """
     print(res_stats_str)
